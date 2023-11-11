3dd6e2324b777dcfe3de51122bf33dd4d10531ce
Emerson Wallace <142413280+loserawr@users.noreply.github.com>
2023-11-10 23:34:10 -0300

    Create funcionario.java

---
+++ Lojacom/funcionario.java
@@ -0,0 +1,27 @@
+package atvpoo;
+public class Funcionario {
+    private String nome;
+    private int rg;
+    private double salario;
+    
+    
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getRg() {
+        return rg;
+    }
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
